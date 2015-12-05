c41d8c531fa3363b940b787b28abe04c59528a04
Gildo Fiorito <fiorito.g@gmail.com>
2015-12-05 14:56:04 +0100

    rename associations on User

--- lib/models.ex
+++ lib/models.ex
@@ -2,8 +2,8 @@ defmodule GithubUnfollowers.User do
   use Ecto.Schema
   schema "users" do
     field :username
-    has_many :following_users, GithubUnfollowers.Relations, foreign_key: :follower_id, on_delete: :fetch_and_delete
-    has_many :followed_user,   GithubUnfollowers.Relations, foreign_key: :followed_id, on_delete: :fetch_and_delete
+    has_many :followers, GithubUnfollowers.Relations, foreign_key: :followed_id, on_delete: :fetch_and_delete
+    has_many :following,   GithubUnfollowers.Relations, foreign_key: :follower_id, on_delete: :fetch_and_delete
 
     timestamps
   end
